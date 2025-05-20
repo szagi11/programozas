b60fe47019bcc3b3006c6af530c0cd83e4833918
szagi11 <agneslilla05@gmail.com>
2025-05-20 21:21:58 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SzotanitoJatek
+{
+    public partial class Form1 : Form
+    {
+        private List<(string magyar, string idegen)> szoparok = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Szótanító játék";
+            this.Width = 800;
+            this.Height = 600;
+            this.AllowDrop = true;
+
+            Button betoltoGomb = new Button() { Text = "Szótár betöltése", Location = new Point(250, 250) };
+            betoltoGomb.Height = 100;
+            betoltoGomb.Width = 200;
+            betoltoGomb.Click += (s, e) =>
+            {
+                BetoltSzotar();
+                KirakSzavakat();
+                betoltoGomb.Visible = false;
+            };
+            this.Controls.Add(betoltoGomb);
+        }
+
+        private void BetoltSzotar()
+        {
+            try
+            {
+                string fajl = Path.Combine(Application.StartupPath, "szotar.txt");
+                if (!File.Exists(fajl))
+                {
+                    MessageBox.Show("A szotar.txt fájl nem található.");
+                    return;
+                }
+
+                szoparok.Clear();
+                foreach (var sor in File.ReadAllLines(fajl))
+                {
+                    var parts = sor.Split(';');
+                    if (parts.Length == 2)
+                        szoparok.Add((parts[0].Trim(), parts[1].Trim()));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba a fájl betöltésekor: " + ex.Message);
+            }
+        }
+
+
+        private void KirakSzavakat()
+        {
+            this.Controls.OfType<Szokartya>().ToList().ForEach(k => this.Controls.Remove(k));
+
+            var rnd = new Random();
+            var kivalasztott = szoparok.OrderBy(x => rnd.Next()).Take(10).ToList();
+
+            List<Szokartya> kartyak = new();
+            for (int i = 0; i < kivalasztott.Count; i++)
+            {
+                kartyak.Add(new Szokartya(kivalasztott[i].magyar, i));
+                kartyak.Add(new Szokartya(kivalasztott[i].idegen, i));
+            }
+
+            kartyak = kartyak.OrderBy(x => rnd.Next()).ToList();
+
+            int x = 20, y = 60;
+            foreach (var k in kartyak)
+            {
+                k.Location = new Point(x, y);
+                k.MouseUp += Kartya_MouseUp;
+                this.Controls.Add(k);
+                y += 40;
+                if (y > 500)
+                {
+                    y = 60;
+                    x += 200;
+                }
+            }
+        }
+
+        private void Kartya_MouseUp(object sender, MouseEventArgs e)
+        {
+            Szokartya aktiv = sender as Szokartya;
+            var cel = this.Controls.OfType<Szokartya>()
+                .FirstOrDefault(k => k != aktiv && k.Bounds.IntersectsWith(aktiv.Bounds));
+
+            if (cel != null && cel.ParID == aktiv.ParID)
+            {
+                this.Controls.Remove(aktiv);
+                this.Controls.Remove(cel);
+
+                if (!this.Controls.OfType<Szokartya>().Any())
+                {
+                    MessageBox.Show("Minden pár megtalálva! 🎉");
+                    KirakSzavakat();
+                }
+            }
+        }
+    }
+}
---
+++ Szokartya.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzotanitoJatek
+{
+    class Szokartya : Label
+    {
+        public int ParID { get; set; }
+        private Point offset;
+        private bool dragging = false;
+
+        public Szokartya(string szoveg, int parID)
+        {
+            this.Text = szoveg;
+            this.ParID = parID;
+            this.AutoSize = true;
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.BackColor = Color.LightYellow;
+
+            this.MouseDown += Szokartya_MouseDown;
+            this.MouseMove += Szokartya_MouseMove;
+            this.MouseUp += Szokartya_MouseUp;
+        }
+
+        private void Szokartya_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                offset = e.Location;
+                dragging = true;
+            }
+        }
+
+        private void Szokartya_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                this.Left += e.X - offset.X;
+                this.Top += e.Y - offset.Y;
+            }
+        }
+
+        private void Szokartya_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+    }
+
+}
